a8b8a4b42dddd4222fb7fcab1eb23e1c54d95b24
RocioAl <koroinice@gmail.com>
2021-04-09 22:37:39 -0500

    corrigiendo ruta de detalle del producto1

--- src/routes/products3.js
+++ src/routes/products3.js
@@ -6,47 +6,47 @@ const products3 = require('../product3.json');
 router.get('/', (req, res) => {
     res.json(products3);
 })
-router.post('/', (req, res) => {
-    const { id, title, image, price, description, sentence } = req.body;
-    if (id && title && image && price && description && sentence) {
-        const id = products3.length + 1;
-        const newProduct = { ...req.body, id };
-        products3.push(newProduct);
-        res.json(products3);
-    } else {
-        res.send('error');
-    }
+// router.post('/', (req, res) => {
+//     const { id, title, image, price, description, sentence } = req.body;
+//     if (id && title && image && price && description && sentence) {
+//         const id = products3.length + 1;
+//         const newProduct = { ...req.body, id };
+//         products3.push(newProduct);
+//         res.json(products3);
+//     } else {
+//         res.send('error');
+//     }
 
-});
-router.delete('/:id', (req, res) => {
-    const { id } = req.params;
-    if (id) {
-        _.each(products3, (product, i) => {
-            if (product.id == id) {
-                products3.splice(i, 1);
-            }
-        });
-        res.json(products3);
-    }
-});
-router.put('/:id', (req, res) => {
-    const { id } = req.params;
-    const { title, image, price, description, sentence } = req.body;
-    if (id && title && image && price && description && sentence) {
-        _.each(products3, (product, i) => {
-            if (product.id === id) {
-                product.id = id;
-                product.title = title;
-                product.image = image;
-                product.price = price;
-                product.description = description;
-                product.sentence = sentence;
-            }
-        });
-        res.json(products3);
-    } else {
-        res.status(500).json({ error: 'error' });
-    }
-});
+// });
+// router.delete('/:id', (req, res) => {
+//     const { id } = req.params;
+//     if (id) {
+//         _.each(products3, (product, i) => {
+//             if (product.id == id) {
+//                 products3.splice(i, 1);
+//             }
+//         });
+//         res.json(products3);
+//     }
+// });
+// router.put('/:id', (req, res) => {
+//     const { id } = req.params;
+//     const { title, image, price, description, sentence } = req.body;
+//     if (id && title && image && price && description && sentence) {
+//         _.each(products3, (product, i) => {
+//             if (product.id === id) {
+//                 product.id = id;
+//                 product.title = title;
+//                 product.image = image;
+//                 product.price = price;
+//                 product.description = description;
+//                 product.sentence = sentence;
+//             }
+//         });
+//         res.json(products3);
+//     } else {
+//         res.status(500).json({ error: 'error' });
+//     }
+// });
 
 module.exports = router;
